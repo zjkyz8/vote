c6738d99504c68bc724102bc7714ffe8ab052af2
zjkyz8 <zjkyz8@gmail.com>
2017-12-25 19:14:03 +0800

    python 版

---
+++ vote.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import time
+import random
+import string
+import requests
+
+while True:
+	print "Start : %s" % time.ctime()
+	openid = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(28))
+	print openid
+	r = requests.post('http://17070.aliyinba.com/Selection/SavaSurvey.ashx', data = {"VoteId": 209, "OpenId": openid, "ItemId":3949})
+	print r.text
+	print "End : %s" % time.ctime()
+	timeSec = random.randint(0, 60);
+	print "sleep %s" % timeSec
+	time.sleep(timeSec)
